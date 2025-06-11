305301d1c41da40ff9a5289dbc27c6bc6c4ba9c3
Ali Wangara <Aliwangara63@gmail.com>
2025-06-11 09:34:35 +0300

    learnt numbers,variables, strings.

---
+++ Scrimba/scrimba-strings.js
@@ -0,0 +1,9 @@
+let welcomeEl = document.getElementById('welcome-el');
+
+let name = 'Ali';
+let greeting ='Welcome back, ';
+
+welcomeEl.innerText = greeting + ' ' + name;
+
+
+welcomeEl.innerText = welcomeEl.innerText + '😊';
