3943d66aafd92b7355e6c256f6ab81dca180a9d5
Ali Wangara <Aliwangara63@gmail.com>
2025-06-23 16:46:37 +0300

    changed a bug on chrome current tab querying

--- Scrimba/chrome-extension/index.js
+++ Scrimba/chrome-extension/index.js
@@ -53,7 +53,7 @@ tabBtn.addEventListener('click',function(){
     //  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     // });
 
-    chrome.tabs.query({active:true, activeWindow:true}, function(tabs){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
 
         myLeads.push(tabs[0].url)
         localStorage.setItem('myLeads', JSON.stringify(myLeads))
